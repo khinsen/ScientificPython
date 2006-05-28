9d7c5a6988fa34bbb059d084089389fcf4f74c2d
"Konrad Hinsen ext:(%22) <konrad.hinsen@cea.fr>
2006-05-28 23:17:33 +0200

    Epydoc docstrings for Statistics

--- Scientific/Statistics/Histogram.py
+++ Scientific/Statistics/Histogram.py
@@ -1,28 +1,19 @@
 # Histograms.
 #
 # Written by Konrad Hinsen <khinsen@cea.fr>
-# last revision: 2005-9-5
+# last revision: 2006-5-28
 #
+
+"""
+Standard and weighted histograms
+"""
 
 from Scientific import N
 
 class Histogram:
 
-    """Histogram in one variable
-
-    Constructor: Histogram(|data|, |bins|, |range|=None)
-
-    Arguments:
-
-    |data| -- a sequence of data points
-
-    |bins| -- the number of bins into which the data is to be sorted
-
-    |range| -- a tuple of two values, specifying the lower and
-               the upper end of the interval spanned by the bins.
-               Any data point outside this interval will be ignored.
-               If no range is given, the smallest and largest
-               data values are used to define the interval.
+    """
+    Histogram in one variable
 
     The bin index and the number of points in a bin can be obtained by
     indexing the histogram with the bin number. Application of len()
@@ -31,6 +22,18 @@ class Histogram:
     """
 
     def __init__(self, data, nbins, range=None):
+        """
+        @param data: a sequence of data points
+        @type data: C{Numeric.array} of C{float} or C{int}
+        @param bins: the number of bins into which the data is to be sorted
+        @type bins: C{int}
+        @param range: a tuple of two values, specifying the lower and
+                      the upper end of the interval spanned by the bins.
+                      Any data point outside this interval will be ignored.
+                      If no range is given, the smallest and largest
+                      data values are used to define the interval.
+        @type range: C{tuple} or C{NoneType}
+        """
         self._setup(data, nbins, range)
         self.addData(data)
 
@@ -47,19 +50,34 @@ class Histogram:
         self.array[:, 0] = self.min + self.bin_width*(N.arange(nbins)+0.5)
 
     def __len__(self):
+        """
+        @returns: the number of bins
+        @rtype: C{int}
+        """
         return self.array.shape[0]
 
     def __getitem__(self, index):
+        """
+        @param index: a bin index
+        @type index: C{int}
+        @returns: an array of shape (2,) containing the bin value and the
+                  bin count
+        """
         return self.array[index]
 
     def __getslice__(self, first, last):
         return self.array[first:last]
 
     def addData(self, data):
-        """Add the values in |data| (a sequence of numbers) to the
-        originally supplied data. Note that this does not affect the
-        default range of the histogram, which is fixed when the
-        histogram is created.
+        """
+        Add values to the originally supplied data sequence. Use this
+        method to feed long data sequences in multiple parts to avoid
+        memory shortages.
+
+        @param data: a sequence of data points
+        @type data: C{Numeric.array}
+        @Note: this does not affect the default range of the histogram,
+               which is fixed when the histogram is created.
         """
         n = (len(data)+999)/1000
         for i in range(n):
@@ -76,34 +94,32 @@ class Histogram:
         self.array[:, 1] =  self.array[:, 1] + histo
 
     def normalize(self, norm=1.):
-        "Scales all counts by the same factor such that their sum is |norm|."
+        """
+        Scale all bin counts by the same factor
+
+        @param norm: the sum of all bin counts after the rescaling
+        @type norm: C{float} or C{int}
+        """
         self.array[:, 1] = norm*self.array[:, 1]/N.add.reduce(self.array[:, 1])
 
     def normalizeArea(self, norm=1.):
-        """Scales all counts by the same factor such that the area under
-        the histogram is |norm|."""
+        """
+        Scale all bin counts by the same factor
+
+        @param norm: the area under the histogram after the rescaling
+        @type norm: C{float} or C{int}
+        """
         self.normalize(norm/self.bin_width)
 
 
 class WeightedHistogram(Histogram):
 
-    """Weighted histogram in one variable
+    """
+    Weighted histogram in one variable
 
     Constructor: WeightedHistogram(|data|, |weights|, |bins|, |range|=None)
 
     Arguments:
-
-    |data| -- a sequence of data points
-
-    |weights| -- a sequence of weights, same length as |data|
-
-    |bins| -- the number of bins into which the data is to be sorted
-
-    |range| -- a tuple of two values, specifying the lower and
-               the upper end of the interval spanned by the bins.
-               Any data point outside this interval will be ignored.
-               If no range is given, the smallest and largest
-               data values are used to define the interval.
 
     In a weighted histogram, each point has a specific weight. If all
     weights are one, the result is equivalent to a standard histogram.
@@ -114,16 +130,35 @@ class WeightedHistogram(Histogram):
     """
 
     def __init__(self, data, weights, nbins, range=None):
+        """
+        @param data: a sequence of data points
+        @type data: C{Numeric.array}
+        @param weights: a sequence of weights, same length as data
+        @type weights: C{Numeric.array}
+        @param bins: the number of bins into which the data is to be sorted
+        @type bins: C{int}
+        @param range: a tuple of two values, specifying the lower and
+                      the upper end of the interval spanned by the bins.
+                      Any data point outside this interval will be ignored.
+                      If no range is given, the smallest and largest
+                      data values are used to define the interval.
+        @type range: C{tuple} or C{NoneType}
+        """
         if len(data) != len(weights):
             raise ValueError("wrong number of weights")
         self._setup(data, nbins, range)
         self.addData(data, weights)
 
     def addData(self, data, weights):
-        """Add the values in |data| (a sequence of numbers) with the
-        given |weights| to the originally supplied data. Note that
-        this does not affect the default range of the histogram, which
-        is fixed when the histogram is created.
+        """
+        Add values to the originally supplied data sequence. Use this
+        method to feed long data sequences in multiple parts to avoid
+        memory shortages.
+
+        @param data: a sequence of data points
+        @type data: C{Numeric.array}
+        @Note: this does not affect the default range of the histogram,
+               which is fixed when the histogram is created.
         """
         if len(data) != len(weights):
             raise ValueError("wrong number of weights")
