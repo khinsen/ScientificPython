e6df851441b0924b8ca6872a70491cbc50601c24
"Konrad Hinsen ext:(%22) <hinsen@cnrs-orleans.fr>
2006-11-15 18:50:50 +0100

    Added documentation for DistributedComputing

--- Scientific/DistributedComputing/MasterSlave.py
+++ Scientific/DistributedComputing/MasterSlave.py
@@ -1,10 +1,48 @@
 #
-# Master/slave process manager for distributed computing
+# Master-slave process manager for distributed computing
 # based on Pyro
 #
 # Written by Konrad Hinsen <hinsen@cnrs-orleans.fr>
-# last revision: 2006-11-14
+# last revision: 2006-11-15
 #
+
+"""
+Distributed computing using a master-slave model
+
+The classes in this module provide a simple way to parallelize independent
+computations in a program. The communication is handled by the Pyro package,
+which must be installed before this module can be used.
+Pyro can be obtained from http://pyro.sourceforge.net/.
+By default, the Pyro name server is used to initialize communication. Please
+read the Pyro documentation for learning how to use the name server.
+
+The principle of the master-slave model is that there is a single master
+process that defines computational tasks and any number of slave processes
+that execute these tasks. The master defines task requests and then waits
+for the results to come in. The slaves wait for a task request, execute it,
+return the result, and wait for the next task. There can be any number of
+slave processes, which can be started and terminated independently, the
+only condition being that no slave process can be started before its master
+process. This setup makes it possible to perform a lengthy computation using
+a variable number of processors.
+
+Communication between the master and the slave processes passes through
+a TaskManager object that is created automatically as part of the master
+process. The task manager stores and hands out task requests and results.
+The task manager also keeps track of the slave processes. When a slave process
+disappears (because it was killed or because of a hardware failure), the
+task manager re-schedules its active task(s) to another slave process. This
+makes the master-slave system very fault tolerant.
+
+Each task manager has a label that makes it possible to distinguish between
+several master-slave groups running at the same time. It is by the label
+that slave processes identify the master process for which they work.
+
+The script "task_manager" prints statistics about a currently active task
+manager; it takes the label as an argument. It shows the number of currently
+active processes (master plus slaves), the number of waiting and running
+tasks, and the number of results waiting to be picked up.
+"""
 
 from Scientific.DistributedComputing.TaskManager import \
                       TaskManager, TaskManagerTermination, TaskRaisedException
@@ -17,8 +55,28 @@ import copy
 import sys
 
 class MasterProcess(object):
+
+    """
+    Master process in a master-slave setup
     
+    A master process in a program is implemented by subclassing
+    this class and overriding the method "run", which calls the methods
+    "requestTask" and "retrieveResult". The process is then
+    launched by calling the method "start".
+    """
+
     def __init__(self, label, use_name_server=True):
+        """
+        @param label: the label that identifies the task manager
+        @type label: C{str}
+        
+        @param use_name_server: If C{True} (default), the task manager is
+                                registered with the Pyro name server. If
+                                C{False}, the name server is not used and
+                                slave processes need to know the host
+                                on which the master process is running.
+        @type use_name_server: C{bool}
+        """
         self.label = label
         self.task_manager = TaskManager()
         self.process_id = self.task_manager.registerProcess()
@@ -30,6 +88,10 @@ class MasterProcess(object):
         self.manager_thread.start()
 
     def taskManagerThread(self):
+        """
+        This method represents the code that is executed in a background
+        thread for remote access to the task manager.
+        """
         self.pyro_daemon=Pyro.core.Daemon()
         if self.pyro_ns is not None:
             self.pyro_daemon.useNameServer(self.pyro_ns)
@@ -53,9 +115,40 @@ class MasterProcess(object):
                 pass
 
     def requestTask(self, tag, *parameters):
-        self.task_manager.addTaskRequest(tag, parameters)
+        """
+        Launches a task request. The task will be executed by a slave
+        process in a method called "do_"+tag that is called with the
+        parameters given in the task request. Note that the order of
+        task executions is not defined.
+
+        @param tag: a tag identifying the computational task. It corresponds
+                    to the name of a method in the slave process.
+        @type tag: C{str}
+
+        @param parameters: the parameters passed to the corresponding method
+                           in the slave process. The only restriction on their
+                           types is that all parameters must be picklable.
+
+        @return: a unique task id
+        @rtype: C{str}
+        """
+        return self.task_manager.addTaskRequest(tag, parameters)
 
     def retrieveResult(self, tag=None):
+        """
+        @param tag: a tag identifying the computational task from which a
+                    return value is requested. If C{None}, results from
+                    any task will be accepted.
+        @type tag: C{str}
+
+        @return: a tuple containing three values: the task id to which the
+                 result corresponds, the tag of the computational task, 
+                 and the result returned by the slave method that handled
+                 the task
+        @rtype: C{tuple}
+
+        @raises L{TaskRaisedException}: if the slave method raised an exception
+        """
         try:
             if tag is None:
                 return self.task_manager.getAnyResult()
@@ -66,6 +159,9 @@ class MasterProcess(object):
             return None, None, None
 
     def start(self):
+        """
+        Starts the master process.
+        """
         try:
             self.run()
         finally:
@@ -76,12 +172,38 @@ class MasterProcess(object):
             self.manager_thread.join()
 
     def run(self):
+        """
+        The main routine of the master process. This method must be
+        overridden in subclasses.
+        """
         raise NotImplementedError
 
 
 class SlaveProcess(object):
 
+    """
+    Slave process in a master-slave setup
+    
+    A concrete slave process in a program is implemented by subclassing
+    this class and adding the methods that handle the computational
+    tasks. Such a method has the name "do_" followed by the tag of the
+    computational task. The process is then launched by calling the
+    method "start".
+    """
+
     def __init__(self, label, master_host=None):
+        """
+        @param label: the label that identifies the task manager
+        @type label: C{str}
+        
+        @param master_host: If C{None} (default), the task manager of the
+                            master process is located using the Pyro name
+                            server. If no name server is used, this parameter
+                            must be the hostname of the machine on which the
+                            master process runs, plus the port number if it
+                            is different from the default (7766).
+        @type master_host: C{str} or C{NoneType}
+        """
         Pyro.core.initClient(banner=False)
         if master_host is None:
             self.task_manager = \
@@ -95,6 +217,10 @@ class SlaveProcess(object):
         self.done = False
 
     def watchdogThread(self):
+        """
+        This method is run in a separate thread that pings the master process
+        regularly to signal that it is still alive.
+        """
         task_manager = copy.copy(self.task_manager)
         while True:
             task_manager.ping(self.process_id)
@@ -103,6 +229,9 @@ class SlaveProcess(object):
             time.sleep(self.watchdog_period)
 
     def start(self):
+        """
+        Starts the slave process.
+        """
         self.process_id = \
             self.task_manager.registerProcess(self.watchdog_period)
         self.background_thread = threading.Thread(target=self.watchdogThread)
--- Scientific/DistributedComputing/TaskManager.py
+++ Scientific/DistributedComputing/TaskManager.py
@@ -2,20 +2,40 @@
 # Task manager for distributed computing based on Pyro
 #
 # Written by Konrad Hinsen <hinsen@cnrs-orleans.fr>
-# last revision: 2006-11-14
+# last revision: 2006-11-15
 #
 
 import Pyro.core
 import threading
 import time
 
+"""
+Task manager for distributed computations. The task manager is used
+by the module MasterSlave, but can also be used directly for different
+distributed computing setups.
+"""
+
 debug = False
 
 class TaskManagerTermination(Exception):
+
+    """
+    Signals that the task manager has no more tasks to handle.
+    """
+
     pass
 
 class TaskRaisedException(Exception):
-    
+
+    """
+    Signals that an exception was raised inside a task. Four
+    attributes provide information about the task and the exception:
+    "task_id" is the task's id, "tag" is its tag, "exception"
+    contains the original exception object, and "traceback"
+    contains a text representation of the stack traceback at the
+    time of the exception.
+    """
+
     def __init__(self, task_id, tag, exception, traceback):
         self.task_id = task_id
         self.tag = tag
@@ -24,7 +44,19 @@ class TaskRaisedException(Exception):
 
 class Task(object):
     
+    """
+    Describes a task inside the task manager.
+    """
+
     def __init__(self, tag, parameters, task_id):
+        """
+        @param tag: the tag of the computational task
+        @type tag: C{str}
+        @param parameters: the parameters of the task
+        @type parameters: C{tuple}
+        @param task_id: the task id
+        @type task_id: C{str}
+        """
         self.tag = tag
         self.parameters = parameters
         self.id = task_id
@@ -36,6 +68,10 @@ class Task(object):
 
 class TaskQueue(object):
 
+    """
+    A FIFO queue for tasks. This class is thread-safe.
+    """
+
     def __init__(self):
         self.tasks = []
         self.tasks_by_tag = {}
@@ -44,9 +80,16 @@ class TaskQueue(object):
         self.terminate = False
 
     def __len__(self):
+        """
+        @returns: the number of tasks in the queue
+        @rtype: C{int}
+        """
         return len(self.tasks)
 
     def terminateWaitingThreads(self):
+        """
+        Makes all threads waiting for a task raise L{TaskManagerTermination}.
+        """
         self.task_available.acquire()
         self.terminate = True
         self.task_available.notifyAll()
@@ -58,6 +101,15 @@ class TaskQueue(object):
             raise TaskManagerTermination()
 
     def addTask(self, task, in_front=False):
+        """
+        @param task: the task to be added
+        @type task: L{Task}
+        @param in_front: if C{True}, add the task at the beginning of the
+                         queue (this is for re-scheduling tasks that were
+                         rejected or not properly handled). Otherwise, add
+                         the task at the end of the queue.
+        @type in_front: C{bool}
+        """
         self.task_available.acquire()
         self.tasks.append(task)
         tasks = self.tasks_by_tag.setdefault(task.tag, [])
@@ -70,6 +122,13 @@ class TaskQueue(object):
         self.task_available.release()
 
     def firstTask(self):
+        """
+        @returns: the first task in the queue
+        @rtype: L{Task}
+
+        Removes the first task from the queue and returns it. If the task queue
+        is empty, the method blocks until a task becomes available.
+        """
         self.task_available.acquire()
         while not self.tasks:
             self._checkForTermination()
@@ -80,6 +139,16 @@ class TaskQueue(object):
         return task
 
     def firstTaskWithTag(self, tag):
+        """
+        @param tag: a task tag
+        @type tag: C{str}
+        @returns: the first task in the queue
+        @rtype: L{Task}
+
+        Removes the first task with the given tag from the queue and returns
+        it. If no task with the requested tag is available, the method blocks
+        until a matching task becomes available.
+        """
         self.task_available.acquire()
         while not self.tasks_by_tag.get(tag, None):
             self._checkForTermination()
@@ -90,6 +159,16 @@ class TaskQueue(object):
         return task
 
     def taskWithId(self, task_id):
+        """
+        @param task_id: a task id
+        @type task_id: C{str}
+        @returns: the task with the given task_id
+        @rtype: L{Task}
+
+        Removes the task with the given task_id from the queue and returns
+        it. If the task is not in the queue, the method blocks
+        until it becomes available.
+        """
         self.task_available.acquire()
         while True:
             task = self.tasks_by_id.get(task_id, None)
@@ -109,6 +188,16 @@ class TaskQueue(object):
 
 class TaskManager(Pyro.core.ObjBase):
 
+    """
+    Manager for computational tasks.
+    
+    A TaskManager accepts task requests and hands them out to other processes.
+    It stores the results that can then be picked up by the requester. A
+    TaskManager also keeps track of its compute processes. If a process
+    disappears, its running tasks are re-scheduled for execution by another
+    compute process. TaskManangers are thread-safe.
+    """
+
     def __init__(self):
         Pyro.core.ObjBase.__init__(self)
         self.id_counter = 0
@@ -123,6 +212,18 @@ class TaskManager(Pyro.core.ObjBase):
         self.watchdog = None
 
     def registerProcess(self, watchdog_period=None):
+        """
+        @param watchdog_period: the period at which the registering process
+                                promises to ping the task manager to signal
+                                that is still alive. If C{None}, no pings
+                                are expected.
+        @type watchdog_period: C{int} or C{NoneType}
+        @returns: a unique process id
+        @rtype: C{int}
+        
+        Registers a process with the task manager. All processes must call
+        this method before making any other task manager calls.
+        """
         self.lock.acquire()
         process_id = self.process_counter
         self.process_counter += 1
@@ -136,6 +237,15 @@ class TaskManager(Pyro.core.ObjBase):
         return process_id
 
     def unregisterProcess(self, process_id):
+        """
+        @param process_id: the id of the process
+        @type process_id: C{int}
+        
+        Removes the process from the task manager's process list. All
+        processes should unregister when they are no longer available
+        for accepting tasks. The task manager will also unregister processes
+        itself if they do not ping the task manager at the promised frequency.
+        """
         if debug:
             print "Unregistering process", process_id
         self.lock.acquire()
@@ -149,13 +259,29 @@ class TaskManager(Pyro.core.ObjBase):
             self.watchdog.unregisterProcess(process_id)
 
     def ping(self, process_id):
+        """
+        @param process_id: the id of the process
+        @type process_id: C{int}
+        
+        Tells the task manager that a process is still alive.
+        """
         if self.watchdog is not None:
             self.watchdog.ping(process_id)
 
     def numberOfActiveProcesses(self):
+        """
+        @returns: the number of active processes
+        @rtype: C{int}
+        """
         return len(self.active_processes)
 
     def numberOfTasks(self):
+        """
+        @returns: a tuple of the number of waiting tasks, the number of
+                  running tasks, and the number of results waiting to be
+                  retrieved.
+        @rtype: C{tuple}
+        """
         self.lock.acquire()
         nwaiting = len(self.waiting_tasks)
         nrunning = len(self.running_tasks)
@@ -164,6 +290,15 @@ class TaskManager(Pyro.core.ObjBase):
         return nwaiting, nrunning, nfinished
 
     def addTaskRequest(self, tag, parameters, process_id=None):
+        """
+        @param tag: the tag of the task being requested
+        @type tag: C{str}
+        @param parameters: the parameters to be passed to the task
+        @param process_id: the id of the requesting process (optional)
+        @type process_id: C{int}
+        @returns: the task id
+        @rtype: C{str}
+        """
         self.lock.acquire()
         task_id = tag + '_' + str(self.id_counter)
         self.id_counter += 1
@@ -178,11 +313,31 @@ class TaskManager(Pyro.core.ObjBase):
         return task_id
 
     def getTaskWithTag(self, tag, process_id=None):
+        """
+        @param tag: a task tag
+        @type tag: C{str}
+        @param process_id: the id of the retrieving process (optional)
+        @type process_id: C{int}
+        @returns: the task id and the parameters
+        @rtype: C{tuple}
+        
+        Returns a waiting task with the given tag. The task is removed from
+        the list of waiting tasks and added to the list of running tasks.
+        """
         task = self.waiting_tasks.firstTaskWithTag(tag)
         self._checkoutTask(task, process_id)
         return task.id, task.parameters
 
     def getAnyTask(self, process_id=None):
+        """
+        @param process_id: the id of the retrieving process (optional)
+        @type process_id: C{int}
+        @returns: the task id, the task tag, and the parameters
+        @rtype: C{tuple}
+        
+        Returns a waiting task of arbitrary tag. The task is removed from
+        the list of waiting tasks and added to the list of running tasks.
+        """
         task = self.waiting_tasks.firstTask()
         self._checkoutTask(task, process_id)
         return task.id, task.tag, task.parameters
@@ -200,6 +355,14 @@ class TaskManager(Pyro.core.ObjBase):
                   % (task.id, str(process_id))
 
     def storeResult(self, task_id, result):
+        """
+        @param task_id: the id of the task for which the result is provided
+        @type task_id: C{str}
+        @param result: the result of the task
+        
+        Stores the result associated with the task. The task is removed from
+        the list of running tasks and added to the list of finished tasks.
+        """
         if debug:
             print "Task %s yielded result %s" % (task_id, result)
         self.lock.acquire()
@@ -212,6 +375,19 @@ class TaskManager(Pyro.core.ObjBase):
         self._removeTask(task)
 
     def storeException(self, task_id, exception, traceback):
+        """
+        @param task_id: the id of the task for which the result is provided
+        @type task_id: C{str}
+        @param exception: the exception raised by the task
+        @param traceback: a text version of the stack traceback at the time
+                          of the exception
+        @type traceback: C{str}
+
+        Stores the exception associated with the task. The task is removed from
+        the list of running tasks and added to the list of finished tasks.
+        When the result is retrieved by another process, L{TaskRaisedException}
+        is raised.
+        """
         if debug:
             print "Task %s raised exception %s" % (task_id, exception)
         self.lock.acquire()
@@ -224,6 +400,14 @@ class TaskManager(Pyro.core.ObjBase):
         self._removeTask(task)
 
     def returnTask(self, task_id):
+        """
+        @param task_id: the id of the task for which the result is provided
+        @type task_id: C{str}
+
+        Removes a task from the list of running tasks and put its back at the
+        beginning of the list of waiting tasks. This method should be called
+        by a process that has obtained a task but cannot handle it.
+        """
         if debug:
             print "Task %s returned" % task_id
         task = self.running_tasks.taskWithId(task_id)
@@ -242,6 +426,13 @@ class TaskManager(Pyro.core.ObjBase):
             self.lock.release()
 
     def getAnyResult(self):
+        """
+        @returns: the task id, the task tag, and the result of the task
+        @rtype: C{tuple}
+        
+        Returns the result of an arbitrary finished task. The task is removed
+        from the list of finished tasks.
+        """
         task = self.finished_tasks.firstTask()
         result = self.results[task.id]
         del self.results[task.id]
@@ -251,6 +442,14 @@ class TaskManager(Pyro.core.ObjBase):
             raise TaskRaisedException(task.id, task.tag, result[0], result[1])
 
     def getResultWithTag(self, tag):
+        """
+        @param tag: a task tag
+        @returns: the task id and the result of the task
+        @rtype: C{tuple}
+        
+        Returns the result of a finished task that has the given tag. The
+        task is removed from the list of finished tasks.
+        """
         task = self.finished_tasks.firstTaskWithTag(tag)
         result = self.results[task.id]
         del self.results[task.id]
@@ -262,6 +461,10 @@ class TaskManager(Pyro.core.ObjBase):
             raise TaskRaisedException(task.id, task.tag, result[0], result[1])
 
     def terminate(self):
+        """
+        Signals that no more tasks or results will be requested. All waiting
+        threads will be terminated by raising L{TaskManagerTermination}.
+        """
         if debug:
             print "Terminating"
         self.waiting_tasks.terminateWaitingThreads()
@@ -270,6 +473,11 @@ class TaskManager(Pyro.core.ObjBase):
 
 
 class Watchdog(object):
+
+    """
+    A background process that watches compute tasks and unregisters those
+    that do not ping the task manager at the promised interval.
+    """
 
     def __init__(self, task_manager):
         self.task_manager = task_manager
